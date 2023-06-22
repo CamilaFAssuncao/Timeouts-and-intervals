c558b1040d51bfa2b4ac140cfef5fa1af808893f
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-06-22 16:08:53 +0200

    timer no style

--- display-time/script.js
+++ display-time/script.js
@@ -1,65 +1,75 @@
 // Exercise 2
 
 // Write a function that displays every second that has passed on the page since it was opened. The display should be refreshed every second. If 60 seconds are elapsed, write "a minute has passed", then "2 minutes have passed" (for 120 seconds and more), etc.
-
+ 
+/* variables for the timer */
 // let seconds = 0;
 // let minutes = 0;
 // let hours = 0;
+// let timer = document.querySelector('.timer');
+// let reset = document.querySelector(".reset");
 
-// let timer = document.querySelector('.demo');
+// setInterval(() => {
+//   seconds += 1;
+//   if (seconds === 60) {
+//     seconds = 0;
+//     minutes++;
+//     if (minutes === 60) {
+//       minutes = 0;
+//       hours++;
+//     }
+//   }
 
 
-// function displayingTime () {
+//   let m = minutes < 10 ? ("0" + minutes) : minutes;
+//   let s = seconds < 10 ? ("0" + seconds) : seconds;
+//   timer.innerHTML = `:${s}`;
 
-// }
+//   if (seconds === 0 && minutes > 0 && minutes % 1 === 0) {
+//     alert(`${minutes} minute(s) have passed!`);
+//   }
+// }, 1000);
 
-/* variables for the timer */
 let seconds = 0;
-let minutes =0;
+let minutes = 0;
 let hours = 0;
-
-let start = document.getElementById('start'); 
-let pause = document.getElementById('pause');
-let reset = document.getElementById('reset'); 
-let stopstatus = 0;
-/* DOM element to update timer */
 let timer = document.querySelector('.timer');
+let message = document.querySelector('.message');
+let reset = document.getElementById("reset");
+let stopstatus; // Variable to store the interval ID
 
+reset.addEventListener('click', () => {
+  clearInterval(stopstatus);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  timer.innerHTML = '00';
+  message.innerHTML = ''; // Clear the message
+  startTimer(); // Start the timer again
+});
 
-
-/* function to start the timer */
-start.addEventListener('click', ()=>{
-    if(stopstatus!==0){
-        clearInterval(stopstatus);
+function startTimer() {
+  stopstatus = setInterval(() => {
+    seconds += 1;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes === 60) {
+        minutes = 0;
+        hours++;
+      }
     }
-    stopstatus = setInterval(()=>{
-        seconds+=1;
-        if(seconds === 60){
-            seconds = 0;
-            minutes++;
-            if(minutes === 60){
-                minutes = 0;
-                hours++;
-            }
-        }
-    let h = hours < 10 ? ("0" + hours ): hours;
+
     let m = minutes < 10 ? ("0" + minutes) : minutes;
-    let s = seconds < 10 ?("0" + seconds) : seconds;
-    timer.innerHTML = ` ${h} : ${m} : ${s}`;
-},1000);
-});
+    let s = seconds < 10 ? ("0" + seconds) : seconds;
+    timer.innerHTML = `${s}`;
 
+    if (seconds === 0 && minutes > 0 && minutes % 1 === 0) {
+      message.innerHTML = `${minutes} minute(s) have passed!`;
+    } else {
+      message.innerHTML = ''; // Clear the message if it's not time yet
+    }
+  }, 1000);
+}
 
-/* function to pause the timer */
-pause.addEventListener('click', ()=>{
-    clearInterval(stopstatus);
-});
-
-/* function to reset the timer */
-reset.addEventListener('click', ()=>{
-    clearInterval(stopstatus);
-    seconds = 0;
-    minutes =0;
-    hours = 0;
-    timer.innerHTML = '00 : 00 : 00 ';
-});
+startTimer(); // Start the timer initially
